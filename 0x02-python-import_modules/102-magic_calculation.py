062e15e88ae973752285ac23bccdbc10f01e98c0
itsoluwatobby <itsoluwatobby@gmail.com>
2023-08-11 22:39:03 +0100

    updated

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-
 def magic_calculation(a, b):
-
+    """outputs the corresponding bytecode provided"""
     from magic_calculation_102 import add, sub
 
     if a < b:
@@ -9,5 +8,6 @@ def magic_calculation(a, b):
         for i in range(4, 6):
             x = add(c, i)
         return (x)
-    
-    return(sub(a, b))
+
+    else:
+        return(sub(a, b))
