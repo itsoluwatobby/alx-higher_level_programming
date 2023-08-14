69fb67711caa959f505c05d5ae58fe5b9453fd21
itsoluwatobby <itsoluwatobby@gmail.com>
2023-08-14 09:28:25 +0100

    done

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > (len(my_list) - ):
-        return None
-    return (my_list[idx])
+    if isinstance(my_list, list):
+        my_list.reverse()
+        for i in my_list:
+            print("{:d}".format(i))
