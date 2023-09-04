8d17119a2386c3da82d1dbbb3fe12e38e40efa97
itsoluwatobby <itsoluwatobby@gmail.com>
2023-09-04 15:04:03 +0100

    done done

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -11,20 +11,20 @@ class Rectangle:
             It takes in two parameter
         
         Args:
-            width = The public Rectangle width
-            height = The private Rectangle height
+            width = The public Rectangle width.
+            height = The private Rectangle height.
         """
         self.width = width
         self.height = height
 
     @property
     def width(self):
-        """ Returns the current width of the Rectangle """
+        """Returns the current width of the Rectangle."""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """ Sets a new width for the Rectangle class """
+        """ Sets a new width for the Rectangle class."""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -33,12 +33,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """ Returns the current height of the Rectangle """
+        """Returns the current height of the Rectangle."""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """ Sets a new height for the Rectangle class """
+        """Sets a new height for the Rectangle class."""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
