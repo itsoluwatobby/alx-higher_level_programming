a2305d8d67215da17b73571e6f5af134ceead776
itsoluwatobby <itsoluwatobby@gmail.com>
2023-09-11 21:18:59 +0100

    a

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -12,6 +12,9 @@ def is_kind_of_class(obj, a_class):
     Args:
         obj - given param to check its instance
         a_class - proposed class instance of obj
+    Returns:
+        If obj is an instance or inherited instance of a_class - True.
+        Otherwise - False.
     """
     if isinstance(obj, a_class):
         return (True)
--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -10,6 +10,9 @@ def inherits_from(obj, a_class):
     Args:
         obj - given param to check its instance
         a_class - proposed class instance of obj
+    Returns:
+        If obj is an inherited instance of a_class - True.
+        Otherwise - False.
     """
     if issubclass(type(obj), a_class) and type(obj) != a_class:
         return (True)
