6068a7f22990bbb56646ddee3e6962c5062dde09
itsoluwatobby <oluwasamuel050@gmail.com>
2024-01-03 16:37:20 +0100

    d

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -13,7 +13,7 @@ const url = process.argv[2];
 
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
-    console.log('code: ', response.statusCode);
+    console.log('code:', response.statusCode);
   } else {
     console.log(error);
   }
