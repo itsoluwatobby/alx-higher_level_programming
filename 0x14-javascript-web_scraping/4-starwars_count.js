149a1047cbff654f8ec50e4cbc2d4a1578b28610
itsoluwatobby <oluwasamuel050@gmail.com>
2024-01-03 10:38:13 +0100

    continuation

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /**
- * a script that prints the title of a Star Wars movie where the
- * episode number matches a given integer.
+ * a script that prints the number of movies where the character
+ * “Wedge Antilles” is present.
  * Requirements:
  *  The first argument is the URL to request (GET)
  *  The status code must be printed like this: code: <status code>
@@ -10,12 +10,16 @@
 
 const request = require('request');
 
-const url = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+const url = 'https://swapi-api.alx-tools.com/api/films';
 
 request(url, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const res = JSON.parse(body);
-    console.log(res.title);
+    let count = 0;
+    res.results.filter((eachMovie) => {
+      return eachMovie.characters.filter((each) => each.includes('18') ? count++ : null);
+    });
+    console.log(count);
   } else {
     console.log(error);
   }
