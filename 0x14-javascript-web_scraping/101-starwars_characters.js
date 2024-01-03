7452b688592d7016885449f3cd1a046f87bbfcc5
itsoluwatobby <oluwasamuel050@gmail.com>
2024-01-03 17:04:47 +0100

    finall

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -7,34 +7,24 @@ const request = require('request');
 
 const id = process.argv[2];
 
-request(`https://swapi-api.alx-tools.com/api/films/${id}`, async (error, response, body) => {
+request(`https://swapi-api.alx-tools.com/api/films/${id}`, (error, response, body) => {
   if (!error && response.statusCode === 200) {
     const result = JSON.parse(body);
     const characters = result.characters;
-    /*Promise.all(characters.map(async (actorUrl) => {
-      await request(actorUrl, (error, response, body) => {
-        if (!error && response.statusCode === 200) {
-          const actor = JSON.parse(body);
-          console.log(actor.name);
-        } else {
-          console.log(error);
-        }
-      });
-    }));*/
-    await makeRequests(characters);
+    makeRequests(characters);
   } else {
     console.log(error);
   }
 });
 
-async function makeRequests(urls) {
+function makeRequests (urls) {
   if (!urls.length) return;
   const url = urls.shift();
-  await request(url, (error, response, body) => {
+  request(url, (error, response, body) => {
     if (!error && response.statusCode === 200) {
       const actor = JSON.parse(body);
       console.log(actor.name);
-      return makeRequests(urls)
+      return makeRequests(urls);
     } else {
       console.log(error);
     }
